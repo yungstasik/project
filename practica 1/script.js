ae22a60bb10cd1c0ba6d0d90687513ac32fbf9a3
Stanislav <krisyukstanvas@icloud.com>
2022-06-24 19:19:36 +0300

    start

---
+++ practica 1/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const number0Films = +prompt('Сколько фильмов вы посмотрели?', '');
+
+const personalMovieDb = {
+    count: number0Films,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt ('Один из последних фильмов?', ''),
+      b = prompt ('На сколько оцените его?', ''),
+      c = prompt ('Один из последних фильмов?', ''),
+      d = prompt ('На сколько оцените его?', '');
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+console.log(personalMovieDb);
+
+
