972f4cfc47cff8e80e589a0569dce0d7c6e79387
Stanislav <krisyukstanvas@icloud.com>
2022-07-08 16:29:31 +0300

    practika3

--- practica 1/script.js
+++ practica 1/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const number0Films = +prompt('Сколько фильмов вы посмотрели?', '');
+let number0Films;
+
+function start () {
+    number0Films = +prompt('Сколько фильмов вы посмотрели?', '');
+
+    while (number0Films == '' || number0Films == null || isNaN(number0Films)) {
+    number0Films = +prompt('Сколько фильмов вы посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDb = {
     count: number0Films,
@@ -17,6 +27,58 @@ const a = prompt ('Один из последних фильмов?', ''),
 
 personalMovieDb.movies[a] = b;
 personalMovieDb.movies[c] = d;
+
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt ('Один из последних фильмов?', ''),
+              b = prompt ('На сколько оцените его?', '');}
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDb.movies[a] = b;
+            console.log('done');
+    } else {
+        console.log('error');
+        i--;
+           
+    } 
+}
+
+rememberMyFilms();
+
+
+function detectPersonalLvl() {
+    if (personalMovieDb.count < 10) {
+        console.log("MALO");
+    } else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+        console.log("NORMALNO");
+    } else if (personalMovieDb.count >= 30) {
+        console.log("EBAT");
+    } else {
+        console.log("error");
+    }
+}
+
+detectPersonalLvl();
+
+function showMyDb (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDb);
+    }
+}
+
+showMyDb(personalMovieDb.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <=3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDb.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
+
 
 console.log(personalMovieDb);
 
